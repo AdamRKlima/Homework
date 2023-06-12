da7635b9608a8b027ff3d9b4b6021a8a6007a9c0
Adam Klima <you@example.comadamrklima@gmail.com>
2023-06-12 17:42:13 -0400

    HW Update

---
+++ section8/week8hw.py
@@ -0,0 +1,23 @@
+import requests
+from dotenv import load_dotenv 
+import pandas as pd 
+import json 
+import os
+
+load_dotenv()
+dict1={}
+api_key=os.getenv('API_KEY')
+# print(api_key)
+# https://api.openweathermap.org/data/2.5/weather?q=Jacksonville&appid=e013c742a8a2882abe22def544e5e770
+try:
+    response=requests.get("https://api.openweathermap.org/data/2.5/weather?q=Jacksonville&appid=e013c742a8a2882abe22def544e5e770")
+    if response.status_code==200:
+        data = response.json()
+        print(data)
+    else:
+        print(f"Request failed. Failure code:",response.status_code)
+except requests.exceptions.RequestException as a:
+    print('Please debug your request. Error response', a)
+
+
+
