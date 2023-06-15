a1eb9234a63ab3d87496957b92da6aa2ee75190c
Adam Klima <adamrklima@gmail.com>
2023-06-15 13:12:15 -0400

    HW Update

--- section8/week8hw.py
+++ section8/week8hw.py
@@ -3,12 +3,12 @@ from dotenv import load_dotenv
 import pandas as pd 
 import json 
 import os
-
+load_dotenv()
 api_key=os.getenv('API_KEY')
 # print(api_key)
 # https://api.openweathermap.org/data/2.5/weather?q=Jacksonville&appid=e013c742a8a2882abe22def544e5e770
 try: 
-    response=requests.get("https://api.openweathermap.org/data/2.5/weather?q=Jacksonville&appid=e013c742a8a2882abe22def544e5e770")
+    response=requests.get(f"https://api.openweathermap.org/data/2.5/weather?q=Jacksonville&appid={api_key}")
     if response.status_code==200:
         data = response.json()
         print(data)
---
+++ section9/Work for HW.sql
@@ -0,0 +1,6 @@
+SELECT *
+FROM NumofSuicByState
+WHERE Year < 2015
+
+DELETE FROM NumofSuicByState
+WHERE Year<2015
